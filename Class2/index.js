dd94276f79d0beba975caee41adae7d766a53faf
SoulMovie <vladgovadovskij@gmail.com>
2026-09-04 12:03:44 +0300

    Add files via upload

---
+++ Class2/index.js
@@ -0,0 +1,107 @@
+/*> < >= <= == === !== !=
+
+let a, b;
+a = "1";
+b=1;
+console.log(a>b);*/
+
+
+/*let temp = Number(prompt('Enter a temperature'));
+
+let result;
+
+if (temp < 0) {
+    result = "холодно";
+}
+else if (temp >= 25) {
+    result = "жарко";
+}
+else if (temp > 0 && temp < 24) {
+    result = "нормально";
+}
+
+alert(result);*/
+
+
+//----------------------------------------------------//
+
+/*let a = Number(prompt("Введіть число"));
+
+if (a % 2 === 0) {
+    alert("Число парне");
+}
+else {
+    alert("Число непарне");
+}*/
+
+//----------------------------------------------------//
+
+
+/*const login = "admin";
+const password = "12345";
+
+let user_login = prompt("Enter your username");
+let user_password = prompt("Enter your password");
+
+if (user_login === login && user_password === password) {
+    alert("Доступ дозволено");
+}
+else {
+    alert("Доступ заборонено");
+}*/
+
+
+//post 100
+//delivery 200
+//pickup 0
+
+
+/*let deliveryType = prompt('Please enter a delivery type');
+let cost = 0
+
+switch (deliveryType) {
+    case 'post':
+        cost = 100;
+        break;
+    case 'delivery':
+        cost = 200;
+        break;
+    case 'pickup':
+        cost = 0;
+        break;
+    default:
+        cost = 'error, wrong delivery type';
+}
+alert(cost);*/
+
+
+//----------------------------------------------------//
+
+/*let number = prompt("Enter a number");
+let week = ""
+
+switch (number) {
+    case "1":
+        week = "monday"
+        break;
+    case "2":
+        week = "tuesday"
+        break;
+    case "3":
+        week = "wednesday"
+        break;
+    case "4":
+        week = "thursday"
+        break;
+    case "5":
+        week = "friday"
+        break;
+    case "6":
+        week = "saturday"
+        break;
+    case "7":
+        week = "sunday"
+        break;
+}*/
+
+//----------------------------------------------------//
