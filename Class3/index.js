2d121a3715090b1f0f74c300997980fa57de906e
SoulMovie <vladgovadovskij@gmail.com>
2026-09-09 13:06:56 +0300

    Add files via upload

---
+++ Class3/index.js
@@ -0,0 +1,66 @@
+/*let age = prompt("What is your age?");
+let access = confirm("Are you registered?");
+
+if(age >= 10 && access === true) {
+    alert("You are logged in!");
+}else{
+    alert("You are not logged in!");
+}*/
+
+
+/*let role = prompt("What is your role?");
+
+if(role === "admin" || role === "teacher") {
+    alert("You are logged in!");
+}else {
+    alert("You are not logged in!");
+}*/
+
+
+/*let login = confirm("Are you registered?")
+let age = prompt("What is your age?");
+
+if (login === true){
+    if (age >= 18){
+        alert("You are logged in!");
+    }else{
+        alert("You are not logged in!");
+    }
+}else{
+    if (age >= 18){
+        alert("Please register!");
+    }else{
+        alert("You are not logged in!");
+    }
+}*/
+
+
+/*let age = prompt("What is your age?");
+
+if (age < 6) {
+    alert("Sweet child o' mine")
+}else if(age >= 6 && age <= 13) {
+    alert("School")
+}else if(age >= 13 && age <= 18){
+    alert("Expired")
+}else if(age >= 18){
+    alert("Adult")
+}*/
+
+
+/*let age = prompt("What is your age?");
+
+if (age >= 18) {
+    alert("Access granted")
+}else if (age < 18 && age >= 16) {
+    let access = confirm("Do you have permission?");
+    if (access === true) {
+        alert("Access granted")
+    }else{
+        alert("Access denied")
+    }
+}else{
+    alert("Access denied")
+}*/
+
+
